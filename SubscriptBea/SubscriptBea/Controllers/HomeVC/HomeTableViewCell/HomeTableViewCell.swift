1680585c031956272ba6709a1d999863322bab90
Harshit Modi <harshitmodi1991@gmail.com>
2021-12-08 04:22:09 -0500

    HM
    Random Image and Title

--- SubscriptBea/SubscriptBea/Controllers/HomeVC/HomeTableViewCell/HomeTableViewCell.swift
+++ SubscriptBea/SubscriptBea/Controllers/HomeVC/HomeTableViewCell/HomeTableViewCell.swift
@@ -38,9 +38,60 @@ class HomeTableViewCell: UITableViewCell {
     
     func configureCell(subscriptionData: Subscription) {
         self.lblTitle.text = subscriptionData.subscriptionTitle
-        self.lblAmount.text = subscriptionData.subscriptionAmount
-        self.lblDueDate.text = subscriptionData.subscriptionType
         
+        if let amountString = subscriptionData.subscriptionAmount {
+            let number = NumberFormatter().number(from: amountString)
+            self.lblAmount.text = number?.commaFormattedAmountStringSingleFraction(showSymbol: true)
+        }
+        
+        self.lblDueDate.text = subscriptionData.subscriptionType
+        self.imgTitle.image = imageWith(name: subscriptionData.subscriptionTitle)
     }
     
+    func imageWith(name: String?) -> UIImage? {
+        let frame = CGRect(x: 0, y: 0, width: vwImage.frame.width, height: vwImage.frame.height)
+        let nameLabel = UILabel(frame: frame)
+        nameLabel.textAlignment = .center
+        nameLabel.backgroundColor = .random()
+        nameLabel.textColor = .white
+        nameLabel.font = UIFont.boldSystemFont(ofSize: 32)
+        var initials = ""
+        if let initialsArray = name?.components(separatedBy: " ") {
+            if let firstWord = initialsArray.first {
+                if let firstLetter = firstWord.first {
+                    initials += String(firstLetter).capitalized }
+            }
+            if initialsArray.count > 1, let lastWord = initialsArray.last {
+                if let lastLetter = lastWord.first { initials += String(lastLetter).capitalized
+                }
+            }
+        } else {
+            return nil
+        }
+        nameLabel.text = initials
+        UIGraphicsBeginImageContext(frame.size)
+        if let currentContext = UIGraphicsGetCurrentContext() {
+            nameLabel.layer.render(in: currentContext)
+            let nameImage = UIGraphicsGetImageFromCurrentImageContext()
+            return nameImage
+        }
+        return nil
+    }
+}
+
+extension CGFloat {
+    static func random() -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UInt32.max)
+    }
+}
+
+extension UIColor {
+    static func random() -> UIColor {
+        return UIColor(
+           red:   .random(),
+           green: .random(),
+           blue:  .random(),
+           alpha: 1.0
+        )
+    }
 }
